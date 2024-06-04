8b9086c2572e36f41371602957131159cd464f8a
Mohammad Mahdi Gholami <gholamimahdi680@gmail.com>
2024-06-05 02:46:38 +0330

    add a score updater that shows the score to player

---
+++ Assets/Scripts/ScoreUpdater.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+public class ScoreUpdater : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+
+    public void UpdateScore(int score){
+        scoreText.text = score.ToString();
+    }
+}
